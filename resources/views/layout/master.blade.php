fd36bac5fb31f3785461c88703475ea3f3c5412a
wahidullah <wahidullah.mdr@gmail.com>
2022-11-13 15:14:18 +0700

    Template uploaded

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" >
+    @include('components.head')
+    <body>
+    @include('components.contents')
+    <!-- @yield('page-contents') -->
+    @include('components.footer')	
+    </body>
+</html>
